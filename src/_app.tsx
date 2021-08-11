c983db62d7497fecd6a08bf8b02d673979ce2ef2
Matt Lim <mlimcollege@gmail.com>
2021-08-10 17:42:20 -0700

    Add packages

---
+++ src/_app.tsx
@@ -0,0 +1,9 @@
+import "@/css/Global.css";
+
+import { AppProps } from "next/app";
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
+}
+
+export default App;
